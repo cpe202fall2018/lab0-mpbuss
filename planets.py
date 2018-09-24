c72b528d7ce5fb43e69fbfbad5ca9e57c0bc888f
Morgan <morganbuss@me.com>
2018-09-24 16:55:04 -0700

    I filled in all of my code for lab 00

--- planets.py
+++ planets.py
@@ -1,7 +1,18 @@
+#
+#Name: Morgan Buss
+#Student ID: 012632655
+#Date (last modified): 09/24/18
+#
+# Lab 00
+# Section 14
+# Purpose of Lab: To practice using python and submitting code via GitHub
+
+# This function takes a user's weight input and displays what they would weigh on Mars and Jupiter
 def weight_on_planets():
-   # write your code here
-   
-   
-   
+    user = float(input("What do you weigh on earth? "))
+    mars = user*0.38
+    jupiter = user*2.34
+    print("\nOn Mars you would weigh %.2f pounds.\nOn Jupiter you would weigh %.2f pounds." % (mars,jupiter))
+
 if __name__ == '__main__':
-   weight_on_planets()
+    weight_on_planets()
